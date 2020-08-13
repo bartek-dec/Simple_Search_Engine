af666eb641d4d3168ebad321f8de7b15117e469b
Bartek Dec <decbartek@gmail.com>
2020-08-13 20:23:18 +0200

    completed stage 4

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,23 +1,32 @@
 package com.example;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+    private static final String path = "./src/main/resources/";
+    private static final String command = "--data";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> commands = Arrays.asList(args);
 
-        System.out.println("Enter the number of people:");
-        int numOfPeople = Integer.parseInt(scanner.nextLine().trim());
-
-        System.out.println("Enter all people:");
-        List<String> people = new ArrayList<>();
-        for (int i = 0; i < numOfPeople; i++) {
-            people.add(scanner.nextLine());
+        int index;
+        String fileName = "";
+        if (commands.contains(command)) {
+            index = commands.indexOf(command);
+            fileName = commands.get(index + 1);
         }
+
+        File file = new File(path + fileName);
+        Reader dataReader = new DataReader();
+        List<String> people = dataReader.readData(file);
 
         Processor processor = new Processor(people, scanner);
         processor.runProgram();
+
     }
 }
---
+++ src/main/java/com/example/Reader.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import java.io.File;
+import java.util.List;
+
+public interface Reader {
+
+    List<String> readData(File file);
+}
